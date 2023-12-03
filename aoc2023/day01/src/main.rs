08187842a2ebb5f2643ce0de8bc4b6766a3c0662
hashankur <aeonian.adroit24@gmail.com>
2023-12-03 20:10:18 +0530

    refactor(aoc2023): day 1

--- aoc2023/day01/src/main.rs
+++ aoc2023/day01/src/main.rs
@@ -12,7 +12,6 @@ fn part1() {
                 nums.push(letter);
             }
         }
-        // println!("{:?}", nums);
         let concat = format!("{}{}", nums[0], (nums[nums.len() - 1]));
         sum += concat.parse::<u32>().unwrap();
     }
@@ -26,7 +25,6 @@ fn part2() {
     ];
     for line in include_str!("input.txt").lines() {
         let mut nums = vec![];
-        let mut lnums = vec![];
 
         for (i, letter) in line.chars().enumerate() {
             if letter.is_numeric() {
@@ -36,22 +34,14 @@ fn part2() {
         for num in spelled_nums {
             let v: Vec<_> = line.match_indices(num).collect();
             if !v.is_empty() {
-                for x in v {
-                    lnums.push(x);
+                for (i, colour) in v.iter() {
+                    let c = spelled_nums.iter().position(|r| r == colour).unwrap();
+                    nums.push((*i, char::from_digit(c as u32 + 1, 10).unwrap()));
                 }
             }
         }
-        // println!("{:?}", line);
-        for (i, colour) in lnums.iter() {
-            let c = spelled_nums.iter().position(|r| r == colour).unwrap();
-            nums.push((*i, char::from_digit(c as u32 + 1, 10).unwrap()));
-        }
         nums.sort();
-        // println!("{:?}", nums);
-        lnums.sort();
-        // println!("{:?}", lnums);
         let concat = format!("{}{}", nums[0].1, (nums[nums.len() - 1].1));
-        // println!("{:?}", concat);
         sum += concat.parse::<u32>().unwrap();
     }
     println!("{sum}");
